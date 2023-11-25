ab8fa2251045f41390078b406f681ffef972a467
Zer0G0ld <matheus321trabalho@gmail.com>
2023-11-25 14:48:54 -0300

    Atualizado e melhorado

--- decodeX.py
+++ decodeX.py
@@ -3,34 +3,53 @@
 # ajudar aqueles que querem aprender sobre criptografia 
 # e os White Hat que visam ajudar a previnir futuros ataques
 
-import sys
+import hashlib
 import os
-import glob
 
-# Adiciona o caminho dos modulos ao PYTHONPATH
-current_dir = os.path.dirname(os.path.abspath(__file__))
-modules_dir = os.path.join(current_dir, 'modules')
-sys.path.append(modules_dir)
+def md5(text):
+    return hashlib.md5(text.encode()).hexdigest()
 
-# Importa todos os módulos da pasta 'Modules' 
-module_files = glob.glob(os.path.join(modules_dir, "*.py"))
+def sha256(text):
+    return hashlib.sha256(text.encode()).hexdigest()
 
-for module_file in module_files:
-    module_name = os.path.splitext(os.path.basename(module_file))[0]
-    __import__ (module_name)
+def sha1(text):
+    return hashlib.sha1(text.encode()).hexdigest()
 
-def menu():
+def break_hash():
+    # Caminho para as pastas
+    current_folder = os.path.dirname(os.path.abspath(__file__))
+    wordlist_folder = os.path.join(current_folder, 'wordlist')
+    hashs_folder = os.path.join(current_folder, 'hashs')
+
+    # Verifica se a pasta existem, se não, cria
+    for folder in [wordlist_folder, hashs_folder]:
+        if not os.path.exist(folder):
+            os.makedirs(folder)
+
+    # Caminho para o arquivo de saida
+
+
+if __name__ == "__main__":
+    output_file = input("Digite o nome do arquivo de saída (incluindo a extensão .txt): ")
+
     print('''
 
     [ 1 ] MD5
     [ 2 ] SHA-256
-    [ 3 ] SHA-1
+    [ 3 ] SHA-17
 
     ''')
 
-    try:
-        inputUser = int(input(">> "))
-    except Exception as Error:
-        print("Erro ao tentar entender o que você digitou : " + str(Error))
+    inputUser = int(input(">> "))
 
-menu()
+    if inputUser == 1:
+        break_hash(output_file, md5)
+
+    elif inputUser == 2:
+        break_hash(output_file, sha256)
+
+    elif inputUser == 3:
+        break_hash(output_file, sha1)
+
+    else:
+        print("Escolha inválida.")
