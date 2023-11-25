e29ae9548f680ed9787646c6dd1bb36a955c8df2
Zer0G0ld <matheus321trabalho@gmail.com>
2023-11-25 16:36:46 -0300

    Add novas funções e organizei tudo, add um wordlist na pasta wordlist

--- decodeX.py
+++ decodeX.py
@@ -1,10 +1,6 @@
-# Authors : Zer0
-# Sobre : Projeto OpenSource de quebra de senha visando
-# ajudar aqueles que querem aprender sobre criptografia 
-# e os White Hat que visam ajudar a previnir futuros ataques
-
 import hashlib
 import os
+import datetime
 
 def md5(text):
     return hashlib.md5(text.encode()).hexdigest()
@@ -15,41 +11,53 @@ def sha256(text):
 def sha1(text):
     return hashlib.sha1(text.encode()).hexdigest()
 
-def break_hash():
+def break_hash(encryption_function):
     # Caminho para as pastas
     current_folder = os.path.dirname(os.path.abspath(__file__))
     wordlist_folder = os.path.join(current_folder, 'wordlist')
     hashs_folder = os.path.join(current_folder, 'hashs')
 
-    # Verifica se a pasta existem, se não, cria
+    # Verifica se a pasta existe, se não, cria
     for folder in [wordlist_folder, hashs_folder]:
-        if not os.path.exist(folder):
+        if not os.path.exists(folder):
             os.makedirs(folder)
 
-    # Caminho para o arquivo de saida
+    # Nome do arquivo de saída baseado na criptografia escolhida e na data atual
+    date_today = datetime.date.today().strftime("%Y-%m-%d")
+    output_file = f"{encryption_function.__name__}_{date_today}.txt"
+    exit_path = os.path.join(hashs_folder, output_file)
 
+    with open(exit_path, 'w') as output_file:
+        for filename in os.listdir(wordlist_folder):
+            wordlist_path = os.path.join(wordlist_folder, filename)
+            with open(wordlist_path, 'r') as word_file:
+                words = [line.strip() for line in word_file.readlines()]
+            
+            for word in words:
+                word_hash = encryption_function(word)
+                output_file.write(f"{word_hash} == {word}\n")
+                print(f"[+] Hash Adicionada: {word_hash} == {word} [+]")
 
 if __name__ == "__main__":
-    output_file = input("Digite o nome do arquivo de saída (incluindo a extensão .txt): ")
-
     print('''
 
     [ 1 ] MD5
     [ 2 ] SHA-256
-    [ 3 ] SHA-17
+    [ 3 ] SHA-1
 
     ''')
 
     inputUser = int(input(">> "))
 
     if inputUser == 1:
-        break_hash(output_file, md5)
+        break_hash(md5)
 
     elif inputUser == 2:
-        break_hash(output_file, sha256)
+        break_hash(sha256)
 
     elif inputUser == 3:
-        break_hash(output_file, sha1)
+        break_hash(sha1)
 
     else:
         print("Escolha inválida.")
+
