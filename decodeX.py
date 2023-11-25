e7dc48f9b0dc7d72052cc9621c639df2891f8f3b
Zer0G0ld <matheus321trabalho@gmail.com>
2023-11-25 11:52:51 -0300

    Corrigi erros

--- decodeX.py
+++ decodeX.py
@@ -1,17 +1,36 @@
+# Authors : Zer0
+# Sobre : Projeto OpenSource de quebra de senha visando
+# ajudar aqueles que querem aprender sobre criptografia 
+# e os White Hat que visam ajudar a previnir futuros ataques
+
 import sys
 import os
 import glob
 
 # Adiciona o caminho dos modulos ao PYTHONPATH
 current_dir = os.path.dirname(os.path.abspath(__file__))
-module_dir = os.path.join(current_dir, 'modules')
+modules_dir = os.path.join(current_dir, 'modules')
 sys.path.append(modules_dir)
 
 # Importa todos os módulos da pasta 'Modules' 
 module_files = glob.glob(os.path.join(modules_dir, "*.py"))
 
 for module_file in module_files:
-    module_name = os.path.splitext(os.path.badename(module_file))[0]
+    module_name = os.path.splitext(os.path.basename(module_file))[0]
     __import__ (module_name)
 
+def menu():
+    print('''
 
+    [ 1 ] MD5
+    [ 2 ] SHA-256
+    [ 3 ] SHA-1
+
+    ''')
+
+    try:
+        inputUser = int(input(">> "))
+    except Exception as Error:
+        print("Erro ao tentar entender o que você digitou : " + str(Error))
+
+menu()
